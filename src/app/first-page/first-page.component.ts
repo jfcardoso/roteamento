cde0128501fc1e477ab8be8ebcc1ef013cf65ace
Jefferson Cardoso <maestro.jfc@gmail.com>
2023-10-23 13:42:52 -0300

    Navegação entre páginas

--- src/app/first-page/first-page.component.ts
+++ src/app/first-page/first-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-first-page',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./first-page.component.css']
 })
 export class FirstPageComponent {
+  
+  constructor(
+    private router: Router
+  ) {}
 
+  goToSecondPage(){
+    this.router.navigate(["second-page"])
+  }
 }
