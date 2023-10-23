36bcd5ba7f9e0297e25ee39a8040596d45724f46
Jefferson Cardoso <maestro.jfc@gmail.com>
2023-10-23 14:12:49 -0300

    Redirecionamento de Urls e PageNotFound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstPageComponent } from './first-page/first-page.component';
 import { SecondPageComponent } from './second-page/second-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: "first-page", component: FirstPageComponent},
-  {path: "second-page", component: SecondPageComponent}
+  {path: "second-page", component: SecondPageComponent},
+  {path: "", redirectTo: "first-page", pathMatch: "full"},
+  {path: "**", component: PageNotFoundComponent}
 ]
 
 @NgModule({
